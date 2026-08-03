54b2c85affe1f86146553342ca225bb9e89d6fb1
crichtiano <crichtianocr777@gmail.com>
2026-08-04 02:33:43 +0300

    orderd

--- js/script.js
+++ js/script.js
@@ -8,48 +8,63 @@ const dark = document.querySelector(".dark-bgc"),
     cance = document.querySelector(".cance"),
     popup = document.querySelector(".popup"),
     login = document.querySelector(".open"),
-    close = document.querySelector("close"),
+    cancell = document.querySelector(".close"),
     black = document.querySelector("dark-btc")
 
+function cancelBurger() {
+    menu.style.left = "-100%";
+    dark.style.display = "none"
+    if (men) {
+        men.style.left = "-100%";
+        dar.style.display = "none"
+    }
+    if (black) {
+        black.style.display = "none"
+    }
+    popup.style.left = "-100%";
+
+}
 
 burger.addEventListener("click", function () {
     menu.style.left = "0";
     dark.style.display = "block"
 })
-burge.addEventListener("click", function () {
-    men.style.left = "0";
-    dar.style.display = "block"
-})
-login.addEventListener("click", function () {
-    popup.style.left = "0";
-    dar.style.display = "block"
-})
-function cancelBurger() {
-    menu.style.left = "-100%";
-    dark.style.display = "none"
-    men.style.left = "-100%";
-    dar.style.display = "none"
-    popup.style.left = "-100%";
-    black.style.display = "none" 
+if (burge) {
+    burge.addEventListener("click", function () {
+        men.style.left = "0";
+        dar.style.display = "block"
+    })
+    cance.addEventListener("click", cancelBurger)
+    dar.addEventListener("click", cancelBurger)
+
 }
+
+login.addEventListener("click", function () {
+    popup.style.left = "50%";
+    popup.style.transform = "translate(-50%, -50%)"
+    dar.style.display = "block"
+})
+
+cancell.addEventListener("click", cancelBurger)
+
 cancel.addEventListener("click", cancelBurger)
 dark.addEventListener("click", cancelBurger)
 
-cance.addEventListener("click", cancelBurger)
-dar.addEventListener("click", cancelBurger)
 
 const cont = document.querySelectorAll(".form")
-    btn = document.querySelectorAll(".btn")
+btn = document.querySelectorAll(".btn")
 
-    btn.forEach(btn => {
-        btn.onclick = () => {
-            cont.forEach(block =>{
-                cont.classlist.remove("active")
-                if(block.classList.contains(btn.dataset.reg)) {
-                    if(!block.classList.contains("active")){
-                        block.classList.add("active")
-                    }
+btn.forEach(activbtn => {
+    activbtn.onclick = (e) => {
+        btn.forEach(b => b.classList.remove("active"))
+        e.target.classList.add("active")
+        cont.forEach(block => {
+            block.classList.remove("active")
+            if (block.classList.contains(activbtn.dataset.reg)) {
+                if (!block.classList.contains("active")) {
+                    block.classList.add("active")
                 }
-            })
-        }
-    });
+            }
+        })
+    }
+});
